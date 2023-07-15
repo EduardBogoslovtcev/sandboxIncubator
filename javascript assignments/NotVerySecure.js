89ac9e09669194867b2d5b6478347b8cbb02def0
EduardBogoslovtcev <edik.bogoslovtcev@gmail.com>
2023-07-15 16:58:03 -0500

    added NotVerySecure

---
+++ javascript assignments/NotVerySecure.js
@@ -0,0 +1,19 @@
+/*
+In this example you have to validate if a user input string is alphanumeric. The given string is not nil/null/NULL/None, so you don't have to check that.
+
+The string has the following conditions to be alphanumeric:
+
+At least one character ("" is not valid)
+Allowed characters are uppercase / lowercase latin letters and digits from 0 to 9
+No whitespaces / underscore
+*/
+
+
+const alphanumeric = (str) => {
+  if (str.length < 1) return false;
+  const ALLOWED_CHARS = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  for (let i=0; i<str.length; i++) {
+    if (!ALLOWED_CHARS.includes(str[i])) return false;
+  }
+  return true;
+};
