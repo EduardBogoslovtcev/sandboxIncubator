592cc6f6b71590b98c2e5a0773e3dcdc1bad3cb8
EduardBogoslovtcev <edik.bogoslovtcev@gmail.com>
2023-07-22 20:10:02 -0500

    added twopositivenums

---
+++ javascript assignments/TwoPositiveNums.js
@@ -0,0 +1,22 @@
+/*
+Task:
+Your job is to write a function, which takes three integers a, b, and c as arguments, and returns True if exactly two of of the three integers are positive numbers (greater than zero), and False - otherwise.
+
+Examples:
+twoArePositive(2, 4, -3) == true
+twoArePositive(-4, 6, 8) == true
+twoArePositive(4, -6, 9) == true
+twoArePositive(-4, 6, 0) == false
+twoArePositive(4, 6, 10) == false
+twoArePositive(-14, -3, -4) == false
+*/
+
+
+function twoArePositive(a, b, c) {
+  let counter = 0;
+  if(a>0) counter++;
+  if(b>0) counter++;
+  if(b>0) counter++;
+  if(counter === 2) return true;
+  return false;
+}
